e2f28efe1f537280d106124a373cb4aedff83f1b
vipingarg7 <54391946+vipingarg7@users.noreply.github.com>
2022-11-02 13:36:14 +0530

    excluded mongoConfiguration

--- src/main/java/com/uniware/integrations/FlipkartApplication.java
+++ src/main/java/com/uniware/integrations/FlipkartApplication.java
@@ -4,9 +4,10 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, MongoAutoConfiguration.class })
 public class FlipkartApplication {
 
 	public static void main(String[] args) {
